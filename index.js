61ee2f879681977b438ef168c7d9afe7b74c68cb
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-08-17 21:01:32 +0100

    progress commit

--- index.js
+++ index.js
@@ -1,66 +1,143 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', ()=> {
 
-const button = document.querySelector('.btn');
-let date = document.querySelector('#dy');
-let month = document.querySelector('#mnth');
-let year = document.querySelector('#yr');
-let inputDD = document.querySelector('#inputDD');
-let inputMM = document.querySelector('#inputMM');
-let inputYY = document.querySelector('#inputYY');
+    const button = document.querySelector('.btn');
+    let date = document.querySelector('#dy');
+    let month = document.querySelector('#mnth');
+    let year = document.querySelector('#yr');
+    let inputYY = ''
+    let inputMM = ''
+    let inputDD = ''
 
-
-const todayDate = new Date();
-const dd = todayDate.getDate();
-const mm = todayDate.getMonth()+1;
-const yy = todayDate.getFullYear();
-
-console.log(dd, mm, yy);
+    const todayDate = new Date();
+   
+    date.textContent = localStorage.getItem("ageDays")
+    month.textContent =  localStorage.getItem("ageMonths") ;
+    year.textContent =  localStorage.getItem("ageYears") ;
 
 
-/*inputDD.addEventListener('input', (event) => 
-{let valDD = event.target.value;
-    date.textContent= valDD;
-});
 
-inputMM.addEventListener('input', (event) =>
-{let valMM = event.target.value;
-    month.textContent = valMM;
-});
+    document.querySelector('#inputDD').value = localStorage.getItem("inputDD")
+    document.querySelector('#inputMM').value = localStorage.getItem("inputMM")
+    document.querySelector('#inputYY').value = localStorage.getItem("inputYY")
 
-inputYY.addEventListener('input', (event) =>
-{let valYY = event.target.value;
-    year.textContent = valYY;
-});*/
-
-    /*date.textContent = inputDD.value;
-
-    inputDD.addEventListener('input', vaDD);
-    inputMM.addEventListener('input', vaMM);
-    inputYY.addEventListener('input', vaYY);
-
-    function vaDD (event) {
-        let valDD = event.target.value;
-        date.textContent= valDD;
-    }
     
-    function vaMM (event){
-        let valMM = event.target.value;
-        month.textContent = valMM;
-    }
+   
     
-    function vaYY(event){
-        let valYY = event.target.value;
-        year.textContent = valYY;
-    }
+
 
     button.addEventListener('click', () => {
-    
+
         
-    });*/
+
+         inputDD = parseInt(document.querySelector('#inputDD').value);
+         inputMM = parseInt(document.querySelector('#inputMM').value);
+         inputYY = parseInt(document.querySelector('#inputYY').value);
+
+
+
+         
+
+            const dd = todayDate.getDate();
+            const mm = todayDate.getMonth()+1;
+            const yy = todayDate.getFullYear();
+
+
+            let ageYears = yy  - inputYY;
+            let ageMonths = mm - inputMM;
+            let ageDays = dd - inputDD;
+        
+
+         console.log(inputDD)
+
+        function validation() {
+            const invalid = document.querySelector('#invalid');
+            const invalides = document.querySelector('#invalides');
+            const invalider = document.querySelector('#invalider');
+    
+            if (!inputDD || !inputMM || !inputYY){
+                invalid.textContent = "Must be a valid date";
+                invalides.textContent = "Must be a valid date";
+                invalider.textContent = "Must be a valid date";
+
+
+                date.textContent = "--";
+                month.textContent =  "--";
+                year.textContent =  "--";
+                return
+
+            }
+            if (inputDD < 1|| inputMM < 1 || inputDD > 31  || inputMM > 12 ){
+                invalid.textContent = "Must aa a valid date";
+                invalides.textContent = "Must aa a valid date";
+
+
+                date.textContent = "--";
+                month.textContent =  "--";
+                year.textContent =  "--";
+                return
+        
+            }
+            
+            if(inputMM === 2 && inputDD > 28 ){
+                invalid.textContent = "Must bb a valid date";
+                invalides.textContent = "Must bb a valid date";
+
+                date.textContent = "--";
+                month.textContent =  "--";
+                year.textContent =  "--";
+                return
+            }
+
+            invalid.textContent = "";
+            invalides.textContent = "";
+            invalider.textContent = "";
+
+
+
+           calculateAge(ageDays, ageMonths, ageYears)
+        } ; 
+
+        validation();
+
+
+
+        function calculateAge(ageDays, ageMonths, ageYears){
+            if ( ageMonths <= 0) {  //|| (ageMonths === 0 && ageDays < 0)
+                ageYears--;
+                
+                ageMonths += 12;
+                
+            }
+    
+            if (ageDays < 0){
+                const daysInLastMonth = new Date(yy,mm-1,0).getDate();
+                // console.log(daysInLastMonth);
+                ageDays += daysInLastMonth;
+                ageMonths--;
+            } 
+
+            localStorage.setItem("ageDays", ageDays);
+            localStorage.setItem("ageMonths", ageMonths);
+            localStorage.setItem("ageYears", ageYears);
+
+
+            localStorage.setItem("inputDD", inputDD);
+            localStorage.setItem("inputMM", inputMM);
+            localStorage.setItem("inputYY", inputYY);
+
+            date.textContent = ageDays;
+            month.textContent =  ageMonths;
+            year.textContent =  ageYears;
+
+            console.log(ageDays, ageMonths, ageYears);
+
+
+        }
+
+       
+       
+            
+        // return {years: ageYears, months: ageMonths, days: ageDays};
+    });
 
 });
-
-
-
-
-

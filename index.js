512e77cd1d29705041b71b0c283e26f8f751a734
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-08-19 18:41:53 +0100

    progress commit

--- index.js
+++ index.js
@@ -1,269 +1,237 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-
+document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('.btn');
-    let date = document.querySelector('#dy');
-    let month = document.querySelector('#mnth');
-    let year = document.querySelector('#yr');
-    let inputYY = ''
-    let inputMM = ''
-    let inputDD = ''
+    const inputD = document.querySelector('#inputDD');
+    const inputM = document.querySelector('#inputMM');
+    const inputY = document.querySelector('#inputYY');
 
-    const todayDate = new Date();
-   
-    date.textContent = localStorage.getItem("ageDays")
-    month.textContent =  localStorage.getItem("ageMonths") ;
-    year.textContent =  localStorage.getItem("ageYears") ;
+    updateAgeContent();
 
-
-
-    document.querySelector('#inputDD').value = localStorage.getItem("inputDD")
-    document.querySelector('#inputMM').value = localStorage.getItem("inputMM")
-    document.querySelector('#inputYY').value = localStorage.getItem("inputYY")
-
-    date.textContent = "--";
-    month.textContent =  "--";
-    year.textContent =  "--";
-
-    
-
-
-    button.addEventListener('click', () => {
-
-        
-
-         inputDD = parseInt(document.querySelector('#inputDD').value);
-         inputMM = parseInt(document.querySelector('#inputMM').value);
-         inputYY = parseInt(document.querySelector('#inputYY').value);
-         
-
-            const dd = todayDate.getDate();
-            const mm = todayDate.getMonth()+1;
-            const yy = todayDate.getFullYear();
-
-
-            let ageYears = yy  - inputYY;
-            let ageMonths = mm - inputMM;
-            let ageDays = dd - inputDD;
-        
-
-         console.log(inputDD)
-
-        function validation() {
-            const invalid = document.querySelector('#invalid');
-            const invalides = document.querySelector('#invalides');
-            const invalider = document.querySelector('#invalider');
-            
-            if (inputDD <1 &&  inputMM <1 && inputYY <1){
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                invalider.textContent = "Must be a valid date";
-                return
-            }
-
-            if (inputDD <1 && inputMM <1 ){
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                return
-            }
-
-            
-            if (inputMM <1 && inputYY <1 ){
-                invalides.textContent = "Must be a valid date";
-                invalid.textContent = "Must be a valid date";
-                return
-            }
-            
-
-            if (inputDD < 1 || inputDD > 31 ){
-                invalid.textContent = "Must be a valid date";
-                return
-        
-            }
-
-            if (inputMM < 1 || inputMM > 12) {
-
-                invalides.textContent = "Must be a valid date";
-
-                date.textContent = "--";
-                month.textContent =  "--";
-                year.textContent =  "--";
-                return
-            }   
-
-            if (inputDD <1 && inputMM <1 ){
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                return
-            }
-            
-            if(inputDD < 1 || inputDD > 31 && inputMM < 1 || inputMM >12) {
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                return 
-            }
-
-            if(!inputDD && !inputMM && !inputYY) {
-                invalid.textContent = "This field is required";
-                invalides.textContent = "This field is required";
-                invalider.textContent = "This field is required";
-
-                return
-            }
-
-            if(!inputDD && !inputMM) {
-                invalid.textContent = "This field is required";
-                invalides.textContent = "This field is required";
-                return
-            }
-            
-            if(!inputDD && !inputYY) {
-                invalid.textContent = "This field is required";
-                invalider.textContent = "This field is required";
-                return
-            }
-
-                        
-            if(!inputMM && !inputYY) {
-                invalides.textContent = "This field is required";
-                invalider.textContent = "This field is required";
-                return
-            }
-    
-            if (!inputDD){
-                invalid.textContent = "This field is required";
-                return
-            }
-            if(!inputMM) {
-                invalides.textContent = "This field is required";
-                return
-            }
-             if(!inputYY) {
-                invalider.textContent = "This field is required";
-                return
-            }
-
-            if(inputDD.toString().length > 2){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if( inputMM.toString().length > 2){
-                invalides.textContent = "Must bb a valid date";
-                return
-            }
-
-            
-            if (inputDD > dd && inputMM > mm && inputYY == yy ) {
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                invalider.textContent = "Must be a valid date";
-                return
-            }  
-
-            if (inputDD > dd && inputMM > mm && inputYY > yy ) {
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                invalider.textContent = "Must be a valid date";
-                return
-            }  
-
-
-            if (inputYY < 1800 || inputYY > yy ) {
-                invalider.textContent = "Must be a valid date";
-                return
-            }   
-
-            if (inputMM > mm && inputYY >= yy ) {
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                invalider.textContent = "Must be a valid date";
-                return
-            }  
-
-            if (inputDD == dd && inputMM == mm && inputYY == yy ) {
-                invalid.textContent = "Must be a valid date";
-                invalides.textContent = "Must be a valid date";
-                invalider.textContent = "Must be a valid date";
-                return
-            }  
-
-            if(inputMM == 2 && inputDD > 28 ){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if(inputMM == 4 && inputDD > 30 ){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if(inputMM == 9 && inputDD > 30 ){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if(inputMM == 6 && inputDD > 30 ){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if(inputMM == 11 && inputDD > 30 ){
-                invalid.textContent = "Must bb a valid date";
-                return
-            }
-
-            if(inputYY.toString().length !== 4){
-                invalider.textContent = "Must bb a valid date";
-                return
-            }
-
-            invalid.textContent = "";
-            invalides.textContent = "";
-            invalider.textContent = "";
-
-           calculateAge(ageDays, ageMonths, ageYears)
-        } ; 
-
-        validation();
-
-
-        function calculateAge(ageDays, ageMonths, ageYears){
-            if ( ageMonths <= 0) {  //|| (ageMonths === 0 && ageDays < 0)
-                ageYears--;
-                
-                ageMonths += 12;
-                
-            }
-    
-            if (ageDays < 0){
-                const daysInLastMonth = new Date(yy,mm-1,0).getDate();
-                // console.log(daysInLastMonth);
-                ageDays += daysInLastMonth;
-                ageMonths--;
-            } 
-
-            localStorage.setItem("ageDays", ageDays);
-            localStorage.setItem("ageMonths", ageMonths);
-            localStorage.setItem("ageYears", ageYears);
-
-
-            localStorage.setItem("inputDD", inputDD);
-            localStorage.setItem("inputMM", inputMM);
-            localStorage.setItem("inputYY", inputYY);
-
-            date.textContent = ageDays;
-            month.textContent =  ageMonths;
-            year.textContent =  ageYears;
-
-            console.log(ageDays, ageMonths, ageYears);
-
-
-        }
-
-       
-       
-            
-        // return {years: ageYears, months: ageMonths, days: ageDays};
+    [inputD, inputM, inputY].forEach(input => {
+        input.addEventListener('input', () => {
+            changeBorderColor(true);
+            changeTextColor(true);
+        });
     });
 
+    function updateAgeContent() {
+        const date = document.querySelector('#dy');
+        const month = document.querySelector('#mnth');
+        const year = document.querySelector('#yr');
+
+        const ageDays = localStorage.getItem('ageDays') || '--';
+        const ageMonths = localStorage.getItem('ageMonths') || '--';
+        const ageYears = localStorage.getItem('ageYears') || '--';
+
+        const currentAgeDays = date.textContent;
+        const currentAgeMonths = month.textContent;
+        const currentAgeYears = year.textContent;
+
+        date.textContent = ageDays;
+        month.textContent = ageMonths;
+        year.textContent = ageYears;
+
+        if (currentAgeDays !== ageDays) {
+            date.classList.add('age-value-animation');
+        }
+        if (currentAgeMonths !== ageMonths) {
+            month.classList.add('age-value-animation');
+        }
+        if (currentAgeYears !== ageYears) {
+            year.classList.add('age-value-animation');
+        }
+
+        setTimeout(() => {
+            date.classList.remove('age-value-animation');
+            month.classList.remove('age-value-animation');
+            year.classList.remove('age-value-animation');
+        }, 5000);
+    }
+
+    function changeBorderColor(valid) {
+        const inpute = document.querySelectorAll('input');
+        inpute.forEach(input => {
+            input.style.borderColor = valid ? 'initial' : 'red';
+        });
+    }
+
+    function changeTextColor(valid) {
+        const textCol = document.querySelectorAll('label');
+        textCol.forEach(label => {
+            label.style.color = valid ? 'initial' : 'red';
+        });
+    }
+
+    button.addEventListener('click', () => {
+        const inputDD = parseInt(inputD.value);
+        const inputMM = parseInt(inputM.value);
+        const inputYY = parseInt(inputY.value);
+
+        const todayDate = new Date();
+        const yy = todayDate.getFullYear();
+
+        const invalid = document.querySelector('#invalid');
+        const invalides = document.querySelector('#invalides');
+        const invalider = document.querySelector('#invalider');
+
+        function showErrorMessages(dayMessage, monthMessage, yearMessage) {
+            invalid.textContent = dayMessage;
+            invalides.textContent = monthMessage;
+            invalider.textContent = yearMessage;
+        }
+
+        function clearErrorMessages() {
+            invalid.textContent = '';
+            invalides.textContent = '';
+            invalider.textContent = '';
+        }
+
+        function isValidDate(day, month, year) {
+            const daysInMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+            return (
+                day >= 1 && day <= daysInMonth[month] + (month === 2 && isLeapYear) &&
+                month >= 1 && month <= 12 &&
+                year >= 1800 && year <= yy
+            );
+        }
+
+        function isFutureDate(day, month, year) {
+            return (
+                year > todayDate.getFullYear() ||
+                (year === todayDate.getFullYear() && (month > todayDate.getMonth() + 1 || (month === todayDate.getMonth() + 1 && day > todayDate.getDate())))
+            );
+        }
+
+        function calculateAge(date) {
+            const diff = todayDate - date;
+            const ageDate = new Date(diff);
+
+            return {
+                years: ageDate.getUTCFullYear() - 1970,
+                months: ageDate.getUTCMonth(),
+                days: ageDate.getUTCDate() - 1
+            };
+        }
+
+        const isDDRequired = !inputDD;
+        const isMMRequired = !inputMM;
+        const isYYRequired = !inputYY;
+
+        if (isDDRequired || isMMRequired || isYYRequired) {
+            showErrorMessages(
+                isDDRequired ? "This field is required" : "",
+                isMMRequired ? "This field is required" : "",
+                isYYRequired ? "This field is required" : ""
+            );
+            changeBorderColor(false);
+            changeTextColor(false);
+        } else if (!isValidDate(inputDD, inputMM, inputYY)) {
+            showErrorMessages(
+                inputDD < 1 || inputDD > 31 ? "Must be a valid day" : "",
+                inputMM < 1 || inputMM > 12 ? "Must be a valid month" : "",
+                inputYY < 1800 || inputYY > yy ? "Must be a valid year" : ""
+            );
+
+            if ([2].includes(inputMM) && inputDD > 28) {
+                showErrorMessages("Must be a valid date", "", "");
+            }
+            if ([4].includes(inputMM) && inputDD > 30) {
+                showErrorMessages("Must be a valid date", "", "");
+            }
+            if ([6].includes(inputMM) && inputDD > 30) {
+                showErrorMessages("Must be a valid date", "", "");
+            }
+            if ([9].includes(inputMM) && inputDD > 30) {
+                showErrorMessages("Must be a valid date", "", "");
+            }
+            if ([11].includes(inputMM) && inputDD > 30) {
+                showErrorMessages("Must be a valid date", "", "");
+            }
+
+            changeBorderColor(false);
+            changeTextColor(false);
+        } else if (isFutureDate(inputDD, inputMM, inputYY)) {
+            showErrorMessages(
+                inputDD > todayDate.getDate() || inputMM > todayDate.getMonth() + 1 || inputYY > todayDate.getFullYear()
+                    ? "The date is in future"
+                    : "",
+                "The date is in the future",
+                "The date is in the future"
+            );
+            changeBorderColor(false);
+            changeTextColor(false);
+        } else {
+            clearErrorMessages();
+
+            changeBorderColor(true);
+            changeTextColor(true);
+
+            const inputDate = new Date(inputYY, inputMM - 1, inputDD);
+            const age = calculateAge(inputDate);
+
+            localStorage.setItem('ageDays', age.days);
+            localStorage.setItem('ageMonths', age.months);
+            localStorage.setItem('ageYears', age.years);
+
+            // localStorage.setItem('inputDD', inputDD);
+            // localStorage.setItem('inputMM', inputMM);
+            // localStorage.setItem('inputYY', inputYY);
+
+            updateAgeContent();
+        }
+    });
 });
+
+ /*const isRequired = !inputDD || !inputMM || !inputYY;
+        const isReq1 = !inputDD;
+        const isReq2 = !inputMM;
+        const isReq3 = !inputYY;
+        const isInvalidDate = !isValidDate(inputDD, inputMM, inputYY);
+        const isInvalid1 = isInvalidDate.getUTCDate();
+        const isInvalid2 = isInvalidDate.getUTCMonth();
+        const isInvalid3 = isInvalidDate.getUTCFullYear();
+
+    
+        if (isRequired) {
+            showErrorMessages("This field is required", "This field is required", "This field is required");
+            changeBorderColor(false);
+            changeTextColor(false);
+        
+         if (!isReq1){
+            showErrorMessages("Must be a valid day", "", "");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+         if (!isReq2) {
+            showErrorMessages("", "Must be a valid month", "");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+         if (!isReq3) {
+            showErrorMessages("", "Must be a valid month", "");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+        }
+        else if (isInvalidDate) {
+            showErrorMessages("Must be a valid day", "Must be a valid month", "Must be a valid year");
+            changeBorderColor(false);
+            changeTextColor(false);
+        
+            if (isInvalid1) {
+            showErrorMessages("Must be a valid day", "", "");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+            if (isInvalid2) {
+            showErrorMessages("", "Must be a valid month", "");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+            if (isInvalid3) {
+            showErrorMessages("", "", "Must be a valid year");
+            changeBorderColor(false);
+            changeTextColor(false);
+            }
+        }*/
